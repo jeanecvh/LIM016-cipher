f55bcd00ad63966ee68e6fbd4dd9f4a071d31c7d
jeanellach <jeanellach@gmail.com>
2021-10-18 22:20:29 -0500

    Javascript on

--- src/index.js
+++ src/index.js
@@ -3,43 +3,69 @@
 import cipher from './cipher.js';
 
 console.log(cipher);*/
-/*
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-/*
-const formularioCifrado = document.getElementById('cifrado');
 
-const textoCifrado = document.getElementById('box2');
+window.addEventListener("load",inicio,true);
 
-const dezplazamiento = document.getElementById('dezplazamiento');
-*/
-let textoparaCifrar = document.getElementById('box1');
-textoparaCifrar.addEventListener("keyup", mayuscula);
-function mayuscula() {
-    this.value=this.value.toUpperCase();
+function inicio(){
+    document.getElementById("mensaje").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true);
+    
+    document.getElementById("cifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;               
+        document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
+    },true);
+    document.getElementById("descifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;                               
+        document.getElementById("mensaje2").value = decifrar(texto, desplazamiento);
+    },true);
 }
 
-let botonCifrar = document.getElementById('cifrar');
-botonCifrar.addEventListener("click", myFunction);
-function myFunction() {
-   let textoparaCifrar = document.getElementById('box1').value;
-   let dezplazamiento = document.getElementById('dezplazamiento').value;
-   document.getElementById("box2").value = botonCifrar(textoparaCifrar, dezplazamiento);
-
-function botonCifrar(textoparaCifrar,dezplazamiento){
-    let resultado = "";
-    let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    dezplazamiento = (dezplazamiento % 26 + 26) % 26;
-
-    if (textoparaCifrar){
-        for (let i = 0; i<textoparaCifrar.lenght; i++){
-            if (letras.indexOF(textoparaCifrar[i])!=-1){
-                let posicion = ((letras.indexOF(textoparaCifrar[i])+dezplazamiento)%26);
-                resultado += letras[posicion];
+function cifrar(texto, desplazamiento) {
+    let resultado='';
+    let letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //Si aceptamos desplazamientos negativos, necesitamos hacerlo dos veces. Si no, sería:
+    //desplazamiento = desplazamiento % 26;
+    desplazamiento = (desplazamiento % 26 + 26) % 26; 
+    
+    if (texto){ //Si el texto es distinto de null (existe)
+        for (let i=0; i<texto.length; ++i){//
+            //Si la letra está en el array de letras (es un símbolo, un espacio...)
+            if (letras.indexOf(texto[i])!=-1)//
+            { 
+                //almacenamos en c la posición de la letra más el desplazamiento y le aplicamos el módulo
+                let posicion=((letras.indexOf(texto[i])+desplazamiento) % 26);
+                resultado+=letras[posicion];
             }
             else
-            resultado += textoparaCifrar[i];
+                resultado+=texto[i]; // Números, espacios, símbolos... 
         }
     }
+    return resultado;
 }
+
+function decifrar(texto, desplazamiento) {
+    let resultado='';
+    let letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //Si aceptamos desplazamientos negativos, necesitamos hacerlo dos veces. Si no, sería:
+    //desplazamiento = desplazamiento % 26;
+    desplazamiento = (desplazamiento % 26 - 26) % 26; 
+    
+    if (texto){ //Si el texto es distinto de null (existe)
+        for (let i=0; i<texto.length; ++i){//
+            //Si la letra está en el array de letras (es un símbolo, un espacio...)
+            if (letras.indexOf(texto[i])!=-1)//
+            { 
+                //almacenamos en c la posición de la letra más el desplazamiento y le aplicamos el módulo
+                let posicion=((letras.indexOf(texto[i])-desplazamiento) % 26);
+                resultado+=letras[posicion];
+            }
+            else
+                resultado+=texto[i]; // Números, espacios, símbolos... 
+        }
+    }
+    return resultado;
 }
+
